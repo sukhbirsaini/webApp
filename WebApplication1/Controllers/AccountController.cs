12aeeb7605321d89e6e6918b89d6c01e015e88a4
sukhbirsaini <sukhbirsaini04@gmail.com>
2017-08-29 12:52:54 +0530

    fail this build

--- WebApplication1/Controllers/AccountController.cs
+++ WebApplication1/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using WebApplication1.Models;
 namespace WebApplication1.Controllers
 {
     [Authorize]
-    public class AccountController : Controller
+    public class Accountntroller : Controller
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
